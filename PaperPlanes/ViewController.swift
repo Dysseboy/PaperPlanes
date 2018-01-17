6642a0b40fffa4b043064d58d81fa018c798d4e7
Dysseboy <alexander.tofilovski@gmail.com>
2018-01-17 15:00:48 +0100

    Initial Commit

---
+++ PaperPlanes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaperPlanes
+//
+//  Created by Alexander Tofilovski on 2018-01-17.
+//  Copyright © 2018 PaperPlanes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
